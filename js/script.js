54e6c0147771331c57ed0f3c404ef7ed1d847265
MaxWofford <max.speed.wofford@gmail.com>
2013-11-08 22:34:01 -0800

    Added index.html and script.js

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ var consinents = [q,w,r,t,y,p,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m]
 
 function myTestFunction()
 {
+//decide parameters of the word
+//var wordL=document.getElementById("test1"); //word length
+//var wordV=document.getElementById("test2"); //which vowels
+//var wordC=document.getElementById("test3"); //which consinents
+//
+////update to what i need
+//wordL.innerHTML=Math.floor((Math.random()*15)+1);
+//wordV.innerHTML=Math.floor((Math.random()*5)+1);
+//wordC.innerHTML=Math.floor((Math.random()*21)+1);
 var x=document.getElementById("demo")
 x.innerHTML=Math.floor((Math.random()*10)+1);
-//decide parameters of the word
-var wordL=document.getElementById("wordL"); //word length
-var wordV=document.getElementById("wordV"); //which vowels
-var wordC=document.getElementById("wordC"); //which consinents
-
-//update to what i need
-wordL.innerHTML=Math.floor((Math.random()*15)+1);
-wordV.innerHTML=Math.floor((Math.random()*5)+1);
-wordC.innerHTML=Math.floor((Math.random()*21)+1);
 }
