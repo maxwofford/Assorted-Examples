344a867950d9e8434788763e3ad9724e245b695d
MaxWofford <max.speed.wofford@gmail.com>
2014-08-15 21:48:52 -0700

    Fixed wrap around bug

--- js/caesar-cipher/caesar-cipher.js
+++ js/caesar-cipher/caesar-cipher.js
@@ -1,10 +1,14 @@
-var input = "abc ?!@"; // Sample input
-var shift = 1;
+var input = "abc ?!@ xyz"; // Sample input
+var shift = 10;
 var charCodeOutput = [];
 for (var i = 0; i <= input.length - 1; i++) {
   
-  if (input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 121 ) { // Alphabet is from 97 to 122
-    charCodeOutput.push(input.charCodeAt(i) + shift); // Shift charcodes of letters 
+  if (input.charCodeAt(i) >= 97 && input.charCodeAt(i) <= 122 ) { // Alphabet is from 97 to 122
+    if (input.charCodeAt(i) + shift > 122) {
+      charCodeOutput.push(96 + (input.charCodeAt(i) + shift)-122);
+    } else{
+      charCodeOutput.push(input.charCodeAt(i) + shift);
+    };
   } else{
     charCodeOutput.push(input.charCodeAt(i)); // Don't shift charcodes of other things (spaces, symbols, puncuation, etc.)
   };
@@ -12,7 +16,7 @@ for (var i = 0; i <= input.length - 1; i++) {
 var output = ""; //clear the output
 console.log(charCodeOutput);
 for (var i =  0; i <= charCodeOutput.length - 1; i++) {
-  console.log("String.fromCharCode(charCodeOutput[i])");
+  console.log(String.fromCharCode(charCodeOutput[i]));
   output = output + String.fromCharCode(charCodeOutput[i]);
 };
 console.log(output);
